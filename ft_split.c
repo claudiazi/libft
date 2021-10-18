283f7ec58406ad8ac7f925857a44858c6b6aa731
claudiazi <claudiazi1244@gmail.com>
2021-10-18 09:03:07 +0200

    Day 7: Debug ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,84 +1,108 @@
 #include "libft.h"
-static int	delimiter_count(char const *s, char c)
+static size_t	ft_count_word(char const *s, char c)
 {
-	int	i;
-	int	count;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
 	count = 0;
 	while (s[i] != '\0')
 	{
-		if (c == s[i])
+		while (s[i] == c && s[i] != '\0')
+			i++;
+		if (s[i] != c)
 			count++;
-		i++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		while (s[i] == c && s[i] != '\0')
+			i++;
 	}
 	return (count + 1);
 }
 
-static char	**initialize_matrix(char const *s, char c, int count)
+static char	*ft_create_word(char const *s, char c)
 {
-	char	**matrix;
-	int		i;
-	int		n;
-	int		m;
+	char	*word;
+	size_t	i;
+	size_t	len;
 
 	i = 0;
-	n = 0;
-	m = 0;
-	if(!(matrix = (char **)malloc(count * sizeof(char *))))
-		return (NULL);
-	while (s[i] != '\0')
+	len = 0;
+	while (s[i] != '\0' && s[i] != c)
 	{
-		if (c == s[i] || s[i + 1] == '\0')
-		{
-			if (!(matrix[m] = (char *)malloc((n + 1) * sizeof(char))))
-			{
-				free(matrix);
-				return (NULL);
-			}
-			matrix[m][n + 1] = '\0';
-			m++;
-			n = -1;
-		}
-		n++;
+		i++;
+		len++;
+	}
+	word = (char *)malloc((len + 1) * sizeof(char));
+	i = 0;
+	while (s[i] != '\0' && s[i] != c)
+	{
+		word[i] = s[i];
 		i++;
 	}
-	return (matrix);
+	word[i] = '\0';
+	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+static void	ft_nfree(char **dst, int n)
 {
-	char	**str;
-	int		count;
-	int		i;
-	int		n;
-	int		m;
+	while (n > 0)
+	{
+		n--;
+		free(dst[n]);
+	}
+	free(dst);
+}
+
+static int	ft_create_array(char **dst, char const *s, char c)
+{
+	size_t	i;
+	size_t	n;
 
 	i = 0;
 	n = 0;
-	m = 0;
-	count = delimiter_count(s, c);
-	str = initialize_matrix(s, c, count);
-	while (m < count)
+	while (s[i] != '\0')
 	{
-		while (s[i] != c && s[i] != '\0')
+		if (s[i] != c)
 		{
-			str[m][n] = s[i];
+			dst[n] = ft_create_word(s, c);
+			if (!dst[n])
+			{
+				ft_nfree(dst, n);
+				return (-1);
+			}
 			n++;
-			i++;
 		}
-		n = 0;
-		m++;
-		i++;
+		while (s[i] != c && s[i] != '\0')
+			s++;
+		while (s[i] == c && s[i] != '\0')
+			s++;
 	}
-	return (str);
+	dst[n] = NULL;
+	return (0);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	char	**dst;
+	size_t	word_count;
+
+	if (!s)
+		return (0);
+	word_count = ft_count_word(s, c);
+	dst = (char **)malloc(sizeof(char *) * word_count);
+	if (dst == 0)
+		return (0);
+	if (ft_create_array(dst, s, c) == -1)
+		return (0);
+	return (dst);
 }
 
 // int	main()
 // {
 // 	int		count = 0;
 // 	char	**str_split = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
-// 	while (count < delimiter_count("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i'))
+// 	while (count < ft_count_word("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i'))
 // 	{
 // 		printf("%s\n", str_split[count]);
 // 		count++;
